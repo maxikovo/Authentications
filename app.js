d1e050fa2ad5215c18334c0f5a0c753918bd973b
maxikov <max_spenser@hotmail.com>
2022-08-27 16:43:22 +0300

    level 4 authentication

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const _= require("lodash");
-const encrypt = require("mongoose-encryption");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 const app = express();
 
 console.log(process.env.API_KEY);
@@ -25,9 +25,6 @@ const userSchema = new mongoose.Schema ({
   password:String
 });
 
-
-
-userSchema.plugin(encrypt,{secret:process.env.SECRET , encryptedFields:["password"]});
 
 const User = new mongoose.model("User",userSchema);
 
@@ -57,21 +54,21 @@ app.post("/",function(req,res){
 });
 
 app.post("/register",function(req,res){
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    const newUser = new User ({
+      email: req.body.username,
+      password:hash
 
-  const newUser = new User ({
-    email: req.body.username,
-    password:md5(req.body.password)
+    });
+    newUser.save(function(err){
+      if(!err){
+        res.render("secrets");
+      }else{
+      console.log(err);
+      }
 
+    });
   });
-  newUser.save(function(err){
-    if(!err){
-      res.render("secrets");
-    }else{
-    console.log(err);
-    }
-
-  });
-
 
 });
 
@@ -83,18 +80,18 @@ User.findOne({
 },function(err,foundUser){
   if(!err){
     if(foundUser){
-      if(foundUser.password === md5(req.body.password) )
-      res.render("secrets");
-    } else {
-      res.redirect("register");
+      bcrypt.compare(req.body.password, foundUser.password , function(err, result) {
+
+if(result === true){
+  res.render("secrets");
+}
+  });
     }
   } else {
     console.log(err);
   }
 
 });
-
-
 });
 
 
